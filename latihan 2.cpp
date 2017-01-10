101bdbbd868668ddcbb859b235aa685953469f3d
Christiankelvinsitorus <christiansitorus955@gmail.com>
2017-01-10 22:09:52 +0700

    latihan 1

---
+++ latihan 2.cpp
@@ -0,0 +1,16 @@
+#include <conio.h>
+#include <iostream.h>
+
+main()
+{
+	cout<<"=====BIODATA====="<<endl;
+	cout<<"NAMA	: CHRISTIAN KELVIN SITORUS"<<endl;
+	cout<<"NIM	: 12165793"<<endl;
+	cout<<"NO HP : 083196371796"<<endl;
+	cout<<"================="<<endl;
+ ;
+	getch();
+}
+
+
+
